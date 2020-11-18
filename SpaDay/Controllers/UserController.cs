851a8aaadeaabf0e4caeacb1d9d1c64b161e2d27
Colin Park <colinpark89@gmail.com>
2020-11-17 18:13:01 -0600

    Studio 14

---
+++ SpaDay/Controllers/UserController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SpaDay.Models;
+
+namespace SpaDay.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("/user")]
+        public IActionResult SubmitAddUserForm(User newUser, string verify)
+        {
+            if (newUser.Password == verify)
+            {
+                ViewBag.UserName = newUser.UserName;
+                return View("Index");
+            }
+            else
+            {
+                ViewBag.Error = "Passwords must match!";
+                ViewBag.UserName = newUser.UserName;
+                ViewBag.Email = newUser.Email;
+
+                return View("Add");
+            }
+
+        }
+    }
+}
---
+++ SpaDay/Models/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SpaDay.Models
+{
+    public class User
+    {
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; }
+
+        public User() { }
+
+        public User(string userName, string email, string password)
+        {
+            UserName = userName;
+            Email = email;
+            Password = password;
+        }
+
+    }
+}
